8a2d7f815d7fbc0773d7e4cd34df7c2bdbf638c6
Milosz Fecko <mfecko@purdue.edu>
2026-08-29 17:05:31 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+      unordered_map<string, vector<int>> groups;
+
+      for (int i = 0; i < strs.size(); i++) {
+        auto s = strs[i];
+        sort(s.begin(), s.end());
+        groups[s].push_back(i);
+      }
+
+      vector<vector<string>> ans;
+
+      for (const auto& [_, group] : groups) {
+        vector<string> strGroup;
+        strGroup.reserve(group.size());
+
+        for (auto idx : group) {
+          strGroup.push_back(strs[idx]);
+        }
+
+        ans.push_back(strGroup);
+      }
+
+      return ans;
+    }
+};
