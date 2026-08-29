af714100613a16d31f1bb12afdc6d53501c2c5b2
Milosz Fecko <mfecko@purdue.edu>
2026-08-29 16:54:40 -0400

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+      unordered_map<int, int> seen;
+      vector<int> ans;
+
+      for (int i = 0; i < nums.size(); i++) {
+        int diff = target - nums[i];
+
+        if (seen.contains(diff)) {
+          ans = {seen[diff], i};
+          break;
+        }
+
+        seen[nums[i]] = i;
+      }
+
+      return ans;
+    }
+};
