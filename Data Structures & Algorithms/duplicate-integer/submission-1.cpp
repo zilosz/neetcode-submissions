fd10033175f6f4eccc3f3c59d3fbb296409a97c7
Milosz Fecko <mfecko@purdue.edu>
2026-08-29 17:17:43 -0400

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+  bool hasDuplicate(vector<int>& nums) {
+    unordered_set<int> uniqueNums;
+
+    for (int n : nums) {
+      if (uniqueNums.contains(n)) return true;
+      uniqueNums.insert(n);
+    }
+
+    return false;
+  }
+};
