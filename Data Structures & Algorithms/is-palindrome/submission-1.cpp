8df00dcb764de2bfc340fd375f80641f4fbd4695
Milosz Fecko <mfecko@purdue.edu>
2026-08-29 18:32:21 -0400

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+  bool isPalindrome(string s) {
+    string str;
+
+    for (char ch : s) {
+      if (isalpha(ch) || isdigit(ch)) {
+        str += tolower(ch);
+      }
+    }
+
+    int n = str.size();
+
+    for (int i = 0; i < n / 2; i++) {
+      if (tolower(str[i]) != tolower(str[n - i - 1])) return false;
+    }
+
+    return true;
+  }
+};
