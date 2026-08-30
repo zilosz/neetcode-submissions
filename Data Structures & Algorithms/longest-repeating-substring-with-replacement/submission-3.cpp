c1b5d1e1a23ae9fb2790d4b793268e25da287a9b
Milosz Fecko <mfecko@purdue.edu>
2026-08-30 12:13:07 -0400

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+  int characterReplacement(string s, int k) {
+     unordered_map<char, int> counter;
+     counter[s[0]] = 1;
+     char mostFreqCh = s[0];
+
+     int l = 0, r = 0, n = s.size(), ans = 0;
+
+     while (true) { 
+       
+       for (r++; r < n && r - l - counter[mostFreqCh] <= k; r++) {
+         if (++counter[s[r]] > counter[mostFreqCh]) {
+           mostFreqCh = s[r];
+         }
+       }
+
+       if (r - l - counter[mostFreqCh] > k) {
+         r--;
+       }
+
+       ans = max(ans, r - l);
+
+       if (r == n) {
+         break;
+       }
+
+       counter[s[l++]]--;
+
+       for (auto [ch, cnt] : counter) {
+         if (cnt > counter[mostFreqCh]) {
+           mostFreqCh = ch;
+         }
+       }
+     }
+
+     return ans;
+  }
+};
