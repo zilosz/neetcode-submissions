9a3601c6e593fc4979a06f579023d65a1919f5ce
Milosz Fecko <mfecko@purdue.edu>
2026-09-04 10:33:14 -0400

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+  int rob(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> robDp(n), skipDp(n);
+
+    robDp[0] = nums[0];
+
+    for (int i = 1; i < n; i++) {
+      int maxSkip = skipDp[i - 1];
+
+      if (i > 1) {
+        maxSkip = max(maxSkip, skipDp[i - 2]);
+      }
+
+      robDp[i] = nums[i] + maxSkip;
+      skipDp[i] = robDp[i - 1];
+    }
+
+    return max(robDp[n - 1], skipDp[n - 1]);
+  }
+};
