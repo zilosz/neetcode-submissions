e3c33fe95d155c624337144710f9ab420928013e
Milosz Fecko <mfecko@purdue.edu>
2026-09-03 22:32:52 -0400

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  unordered_map<int, int> memo;
+
+public:
+  int climbStairs(int n) {
+    if (n < 0) return 0;
+    if (n == 0) return 1;
+    if (memo.contains(n)) return memo[n];
+    return memo[n] = climbStairs(n - 1) + climbStairs(n - 2);
+  }
+};
