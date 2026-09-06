69aaed7c974adb214096abd3e3bbf9b199e7c414
Milosz Fecko <mfecko@purdue.edu>
2026-09-06 19:26:56 -0400

    Add: merge-intervals - submission-3

---
+++ Data Structures & Algorithms/merge-intervals/submission-3.cpp
@@ -0,0 +1,31 @@
+bool intervalCmp(const vector<int>& a, const vector<int>& b) {
+  return a[0] < b[0] || a[0] == b[0] && a[1] < b[1];
+}
+
+class Solution {
+public:
+  vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    sort(intervals.begin(), intervals.end(), intervalCmp);
+    for (auto& ez : intervals) {
+        cout << format("{} {}\n", ez[0], ez[1]);
+    }
+
+    vector<vector<int>> merged;
+    int i = 0, n = intervals.size();
+
+    while (i < n) {
+      int j = i, end = intervals[i][1];
+      
+      while (j < n - 1 && end >= intervals[j + 1][0]) {
+        end = max(end, intervals[j + 1][1]);
+        j++;
+      }
+
+      merged.push_back({intervals[i][0], end});
+
+      i = j + 1;
+    }
+
+    return merged;
+  }
+};
