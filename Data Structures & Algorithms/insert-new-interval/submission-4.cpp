90932f7df7b3428e46d6455e221bd885f80ca9af
Milosz Fecko <mfecko@purdue.edu>
2026-09-08 21:32:30 -0400

    Add: insert-new-interval - submission-4

---
+++ Data Structures & Algorithms/insert-new-interval/submission-4.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+  vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+    vector<vector<int>> mergedIntervals;
+    int i = 0, n = intervals.size();
+
+    for (; i < n && newInterval[0] > intervals[i][1]; i++) {
+      mergedIntervals.push_back(intervals[i]);
+    }
+
+    if (i == n || newInterval[1] < intervals[i][0]) {
+      mergedIntervals.push_back(newInterval);
+    } else {
+      int start = min(newInterval[0], intervals[i][0]);
+      int end = max(newInterval[1], intervals[i][1]);
+
+      for (i++; i < n && intervals[i][0] <= end; i++) {
+        end = max(end, intervals[i][1]);
+      }
+
+      mergedIntervals.push_back({start, end});
+    }
+
+    for (; i < n; i++) {
+      mergedIntervals.push_back(intervals[i]);
+    }
+
+    return mergedIntervals;
+  }
+};
