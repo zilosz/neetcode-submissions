60538a64bc47281993f0a791786bfcf54054bd44
Milosz Fecko <mfecko@purdue.edu>
2026-09-18 15:30:14 -0400

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+  vector<int> twoSum(vector<int>& numbers, int target) {
+    int i = 0, j = numbers.size() - 1;
+
+    while (true) {
+      int sum = numbers[i] + numbers[j];
+
+      if (sum == target) {
+        break;
+      }
+
+      if (sum < target) {
+        i++;
+      } else {
+        j--;
+      }
+    }
+
+    return {i + 1, j + 1};
+  }
+};
